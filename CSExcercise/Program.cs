b39fdb839e87abbf1fd588686fe558323f8a8899
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-08-11 13:24:50 +0200

    Initial upload

---
+++ CSExcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
